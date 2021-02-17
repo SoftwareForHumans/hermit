bd02501f64a1c0b8c3e4a60a648a2fd11c85dee1
João Carlos Maduro <joao-carlos.14@hotmail.com>
2021-02-17 21:49:58 +0000

    Added more dockerfile examples

--- src/modules/image.ts
+++ src/modules/image.ts
@@ -1,7 +1,9 @@
+import { languageMap } from '../languages/javascript'
+
 const imageModule = (): Array<string> => {
   //TODO: Develop image module
 
-  return ["node:14", "gcr.io/distroless/nodejs:14"];
+  return [languageMap.imageBuilder, languageMap.imageRunner];
 }
 
 export default imageModule;
