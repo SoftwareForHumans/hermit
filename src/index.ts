ce1d40b7e24b7e1618cc80572840549fabf68d70
João Carlos Maduro <joao-carlos.14@hotmail.com>
2021-03-10 23:55:52 +0000

    Fixed hermit

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,19 @@ import languageModule from './modules/languages'
 import DockerfileData from './utils/lib/DockerfileData';
 import HermitOptions from './utils/lib/HermitOptions';
 
+// Default Options
 export const defaultOptions: HermitOptions = {
   multiStage: false,
   timeout: 60
 };
 
-export const dockerfileGeneration = async (command: string, options: HermitOptions = defaultOptions) => {
+export const dockerfileGeneration = async (command: string, suppliedOptions: any = {}) => {
+  // Set the options
+  const options: HermitOptions = {
+    multiStage: suppliedOptions.multiStage || defaultOptions.multiStage,
+    timeout: suppliedOptions.timeout || defaultOptions.timeout
+  };
+
   // Modules to analyse software data
   const inspectedData = await inspectorModule();
   const tracedData = await tracerModule(command, options);
@@ -24,10 +31,10 @@ export const dockerfileGeneration = async (command: string, options: HermitOptio
   const languageData = await languageModule(inspectedData.language);
 
   // Modules to infer dockerfiles fields
-  const imageData = imageModule(inspectedData, tracedData, languageData);
-  const dependenciesData = dependenciesModule(inspectedData, tracedData, languageData);
-  const portsData = portsModule(inspectedData, tracedData, languageData);
-  const entrypointData = entrypointModule(inspectedData, tracedData, languageData);
+  const imageData = imageModule(inspectedData, tracedData, languageData, options);
+  const dependenciesData = dependenciesModule(inspectedData, tracedData, languageData, options);
+  const portsData = portsModule(inspectedData, tracedData, languageData, options);
+  const entrypointData = entrypointModule(inspectedData, tracedData, languageData, options);
 
   // Join Dockerfile Data
   const dockerfileData: DockerfileData = {
